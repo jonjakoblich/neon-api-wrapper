201dd48bed444a2002c3bd1b61d11f885ac03c35
Jon Jakoblich <jjakoblich@fadica.org>
2024-12-04 10:29:03 -0500

    Add architecture tests

---
+++ tests/ArchitectureTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use TwoJays\NeonApiWrapper\Contracts\DataObject;
+use TwoJays\NeonApiWrapper\Data;
+
+uses()->group('arch');
+
+arch()
+    ->expect('App')
+    ->not->toUse(['die', 'dd', 'dump']);
+
+arch()
+    ->expect('TwoJays\NeonApiWrapper\Attributes')
+    ->toBeClasses()
+    ->toHaveAttribute(Attribute::class);
+
+arch()
+    ->expect('TwoJays\NeonApiWrapper\Concerns')
+    ->toBeTraits();
+
+arch()
+    ->expect('TwoJays\NeonApiWrapper\Contracts')
+    ->toBeInterfaces();
+
+arch()
+    ->expect('TwoJays\NeonApiWrapper\DataObjects')
+    ->toBeClasses()
+    ->toExtend(Data::class)
+    ->toImplement(DataObject::class);
+
+arch()
+    ->expect('TwoJays\NeonApiWrapper\Enums')
+    ->toBeStringBackedEnums();
+
+arch()
+    ->expect('TwoJays\NeonApiWrapper\Exceptions')
+    ->toBeClasses()
+    ->toExtend(Exception::class);
