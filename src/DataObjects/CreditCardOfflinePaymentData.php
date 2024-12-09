9528cb63a4d912bd86d1176980a510e1decc53c6
Jon Jakoblich <jjakoblich@fadica.org>
2024-12-09 12:35:24 -0500

    Allow null values

--- src/DataObjects/CreditCardOfflinePaymentData.php
+++ src/DataObjects/CreditCardOfflinePaymentData.php
@@ -7,12 +7,12 @@ use TwoJays\NeonApiWrapper\Data;
 class CreditCardOfflinePaymentData extends Data
 {
     public function __construct(
-        public string $cardNumberLastFour,
-        public int $expirationMonth,
-        public int $expirationYear,
-        public string $cardTypeCode,
-        public string $cardHolderName,
-        public string $cardHolderEmail,
-        public BillingAddressData $billingAddress
+        public ?string $cardNumberLastFour,
+        public ?int $expirationMonth,
+        public ?int $expirationYear,
+        public ?string $cardTypeCode,
+        public ?string $cardHolderName,
+        public ?string $cardHolderEmail,
+        public ?BillingAddressData $billingAddress
     ) {}
 }
--- src/DataObjects/CreditCardOnlinePaymentData.php
+++ src/DataObjects/CreditCardOnlinePaymentData.php
@@ -7,14 +7,14 @@ use TwoJays\NeonApiWrapper\Data;
 class CreditCardOnlinePaymentData extends Data
 {
     public function __construct(
-        public string $token,
-        public string $cardNumberLastFour,
-        public int $expirationMonth,
-        public int $expirationYear,
-        public string $cardTypeCode,
-        public string $cardHolderName,
-        public string $cardHolderEmail,
-        public BillingAddressData $billingAddress,
-        public string $transactionNumber
+        public ?string $token,
+        public ?string $cardNumberLastFour,
+        public ?int $expirationMonth,
+        public ?int $expirationYear,
+        public ?string $cardTypeCode,
+        public ?string $cardHolderName,
+        public ?string $cardHolderEmail,
+        public ?BillingAddressData $billingAddress,
+        public ?string $transactionNumber
     ) {}
 }
