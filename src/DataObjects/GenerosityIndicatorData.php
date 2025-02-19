2a9d9cbb999281e369403cc33b41a5f18f7637f2
Jon Jakoblich <jjakoblich@fadica.org>
2025-02-19 16:52:04 -0500

    fix: Make tomse parameters optional

--- src/DataObjects/GenerosityIndicatorData.php
+++ src/DataObjects/GenerosityIndicatorData.php
@@ -7,10 +7,10 @@ use TwoJays\NeonApiWrapper\Data;
 class GenerosityIndicatorData extends Data
 {
     public function __construct(
-        public float $indicator,
-        public int $affinity,
-        public int $recency,
-        public int $frequency,
-        public int $monetaryValue
+        public ?float $indicator,
+        public ?int $affinity,
+        public ?int $recency,
+        public ?int $frequency,
+        public ?int $monetaryValue
     ) {}
 }
